e5a1227149afc76179afeb30b5cfa7829606eca0
nardswolfs <nard.swolfs@hotmail.com>
2016-10-07 11:12:04 +0200

    tcpconnector

--- EindOpdrachtCsharp/ConnectionManagers/TCPConnector.cs
+++ EindOpdrachtCsharp/ConnectionManagers/TCPConnector.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,19 +11,23 @@ namespace EindOpdrachtCsharp.ConnectionManagers
 {
     class TCPConnector
     {
+        private Stream stream;
+
         public TCPConnector(TcpClient client)
         {
-            
+            stream = client.GetStream();
         }
 
         public void sendData(object data)
         {
-            
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(stream, data);
         }
 
         public object checkForData()
         {
-            
+            BinaryFormatter bf = new BinaryFormatter();
+            return bf.Deserialize(stream);
         }
     }
 }
