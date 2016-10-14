4edf085f166754866edf73249915108404d4d9c2
Nard <nard.swolfs@hotmail.com>
2016-10-14 14:19:16 +0200

    made a nicer GUI

--- EindOpdrachtCsharp/GameGUI.Designer.cs
+++ EindOpdrachtCsharp/GameGUI.Designer.cs
@@ -28,16 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ListViewItem listViewItem5 = new System.Windows.Forms.ListViewItem("Player1:  1000 (3 pogingen)");
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem("Player1:  1000 (3 pogingen)");
             this.drawPanel = new System.Windows.Forms.Panel();
             this.StateLabel = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.highScores = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button5 = new System.Windows.Forms.Button();
             this.colorPanel = new System.Windows.Forms.Panel();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.groupBox3 = new System.Windows.Forms.GroupBox();
             this.button4 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
@@ -50,12 +50,12 @@
             this.clearPanel = new System.Windows.Forms.Button();
             this.selectItems = new System.Windows.Forms.ListView();
             this.Option = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button5 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
-            this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // drawPanel
@@ -80,7 +80,7 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.highScores);
-            this.groupBox1.Location = new System.Drawing.Point(201, 473);
+            this.groupBox1.Location = new System.Drawing.Point(336, 473);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(252, 157);
             this.groupBox1.TabIndex = 3;
@@ -96,7 +96,7 @@
             this.columnHeader1});
             this.highScores.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
             this.highScores.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
-            listViewItem5});
+            listViewItem1});
             this.highScores.Location = new System.Drawing.Point(7, 27);
             this.highScores.MultiSelect = false;
             this.highScores.Name = "highScores";
@@ -108,82 +108,91 @@
             // 
             // groupBox2
             // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
             this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button2);
             this.groupBox2.Controls.Add(this.colorPanel);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.groupBox2.Location = new System.Drawing.Point(0, 473);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(83, 157);
+            this.groupBox2.Size = new System.Drawing.Size(330, 157);
             this.groupBox2.TabIndex = 4;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Brush";
             // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(12, 117);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(82, 34);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Eraser";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
             // colorPanel
             // 
             this.colorPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.colorPanel.Location = new System.Drawing.Point(25, 25);
+            this.colorPanel.Location = new System.Drawing.Point(12, 27);
             this.colorPanel.Name = "colorPanel";
-            this.colorPanel.Size = new System.Drawing.Size(35, 28);
+            this.colorPanel.Size = new System.Drawing.Size(82, 45);
             this.colorPanel.TabIndex = 5;
             this.colorPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.colorPanel_Paint);
             // 
-            // groupBox3
-            // 
-            this.groupBox3.Controls.Add(this.button4);
-            this.groupBox3.Controls.Add(this.button1);
-            this.groupBox3.Controls.Add(this.button3);
-            this.groupBox3.Controls.Add(this.button2);
-            this.groupBox3.Location = new System.Drawing.Point(89, 473);
-            this.groupBox3.Name = "groupBox3";
-            this.groupBox3.Size = new System.Drawing.Size(106, 157);
-            this.groupBox3.TabIndex = 5;
-            this.groupBox3.TabStop = false;
-            this.groupBox3.Text = "Shapes";
-            // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(6, 121);
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(113, 90);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.Size = new System.Drawing.Size(95, 59);
             this.button4.TabIndex = 8;
-            this.button4.Text = "draw";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Text = "Brush";
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(6, 26);
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(113, 25);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.Size = new System.Drawing.Size(95, 59);
             this.button1.TabIndex = 7;
-            this.button1.Text = "lijn";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "Line";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(6, 58);
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(230, 25);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.Size = new System.Drawing.Size(94, 59);
             this.button3.TabIndex = 6;
-            this.button3.Text = "rechthoek";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Text = "Rectangle";
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(6, 93);
+            this.button2.BackColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(230, 90);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(94, 27);
+            this.button2.Size = new System.Drawing.Size(94, 59);
             this.button2.TabIndex = 0;
-            this.button2.Text = "driehoek";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Text = "Triangle";
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // groupBox4
             // 
             this.groupBox4.Controls.Add(this.playerCount);
             this.groupBox4.Controls.Add(this.sessionDetails);
-            this.groupBox4.Location = new System.Drawing.Point(459, 473);
+            this.groupBox4.Location = new System.Drawing.Point(594, 473);
             this.groupBox4.Name = "groupBox4";
             this.groupBox4.Size = new System.Drawing.Size(192, 157);
             this.groupBox4.TabIndex = 6;
@@ -202,6 +211,7 @@
             // sessionDetails
             // 
             this.sessionDetails.AutoSize = true;
+            this.sessionDetails.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.sessionDetails.Location = new System.Drawing.Point(27, 33);
             this.sessionDetails.Name = "sessionDetails";
             this.sessionDetails.Size = new System.Drawing.Size(80, 20);
@@ -212,7 +222,7 @@
             // 
             this.listBox2.FormattingEnabled = true;
             this.listBox2.ItemHeight = 20;
-            this.listBox2.Location = new System.Drawing.Point(27, 26);
+            this.listBox2.Location = new System.Drawing.Point(9, 25);
             this.listBox2.Name = "listBox2";
             this.listBox2.Size = new System.Drawing.Size(158, 124);
             this.listBox2.TabIndex = 7;
@@ -220,7 +230,7 @@
             // groupBox5
             // 
             this.groupBox5.Controls.Add(this.listBox2);
-            this.groupBox5.Location = new System.Drawing.Point(669, 473);
+            this.groupBox5.Location = new System.Drawing.Point(792, 473);
             this.groupBox5.Name = "groupBox5";
             this.groupBox5.Size = new System.Drawing.Size(214, 157);
             this.groupBox5.TabIndex = 8;
@@ -229,12 +239,13 @@
             // 
             // clearPanel
             // 
-            this.clearPanel.Location = new System.Drawing.Point(919, 485);
+            this.clearPanel.BackColor = System.Drawing.Color.White;
+            this.clearPanel.Location = new System.Drawing.Point(985, 486);
             this.clearPanel.Name = "clearPanel";
-            this.clearPanel.Size = new System.Drawing.Size(133, 55);
+            this.clearPanel.Size = new System.Drawing.Size(103, 138);
             this.clearPanel.TabIndex = 9;
             this.clearPanel.Text = "Clear Panel";
-            this.clearPanel.UseVisualStyleBackColor = true;
+            this.clearPanel.UseVisualStyleBackColor = false;
             this.clearPanel.Click += new System.EventHandler(this.clearPanel_click);
             // 
             // selectItems
@@ -259,39 +270,42 @@
             // 
             this.Option.Width = 100;
             // 
-            // button5
+            // numericUpDown1
             // 
-            this.button5.Location = new System.Drawing.Point(6, 123);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 34);
-            this.button5.TabIndex = 6;
-            this.button5.Text = "gum";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 79);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(82, 26);
+            this.numericUpDown1.TabIndex = 7;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
             // 
             // GameGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
             this.ClientSize = new System.Drawing.Size(1355, 642);
             this.Controls.Add(this.selectItems);
             this.Controls.Add(this.clearPanel);
             this.Controls.Add(this.groupBox5);
             this.Controls.Add(this.groupBox4);
-            this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.StateLabel);
             this.Controls.Add(this.drawPanel);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "GameGUI";
             this.Text = "GameGUI";
             this.Load += new System.EventHandler(this.GameGUI_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
-            this.groupBox3.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
             this.groupBox5.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -305,7 +319,6 @@
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Panel colorPanel;
         private System.Windows.Forms.ColorDialog colorDialog1;
-        private System.Windows.Forms.GroupBox groupBox3;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox4;
@@ -321,5 +334,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
     }
 }
